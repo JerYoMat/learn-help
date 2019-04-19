0f34d4bb09796b9b9a4e8fdc5f0d7f2ce70308ba
JerYOMat <jeremy.emata@gmail.com>
2019-04-19 17:27:34 -0400

    refactor

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,7 +8,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
