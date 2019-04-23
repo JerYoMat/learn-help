9f1ebe3c411ca6f7ca77f1bd0b94742645a10520
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 11:51:50 -0400

    added User.digest method;

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,12 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255} 
   validates :email, presence: true, length: { maximum: 255}, format:  { with: VALID_EMAIL_REGEX }, uniqueness:{ case_sensitive: false }
   validates :password, presence: true, confirmation: true, length: { minimum: 6 }, allow_nil: true 
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? 
+      BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
   
 end
