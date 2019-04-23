6ff34e560256646dc5fbf3e2ca1a3d0c8fa66104
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 17:52:46 -0400

    added mirror methods here that act similarly to session helper -> review required

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,7 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
+  
   
   def logged_in?
     !current_user.nil?
@@ -17,5 +13,41 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  def remember(user)
+    user.remember #creates new token, hashes it and sets as remember_token via update_attribute
+    # Sets an encrypted cookie value before sending it to the client which
+    # prevent users from reading and tampering with its value.
+    # The cookie is signed by your app's `secrets.secret_key_base` value.
+    # It can be read using the encrypted method `cookies.encrypted[:name]`
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end 
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+  
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Logs out the current user.
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,11 +5,31 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
   def is_logged_in?
     !session[:user_id].nil?
   end
-  # Add more helper methods to be used by all tests here...
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password1', remember_me: '1')
+    post login_path, params: {
+      session: {
+        email: user.email,
+        password: password,
+        remember_me: remember_me 
+        } 
+      }
+  end
+
+  def forget(user)
+    user.update_attribute(:remember_digest, nil)
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end 
 end
