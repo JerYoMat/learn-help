acd5884828490d483115224b31d6c0d04613a209
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 05:53:56 -0400

    created user model, added validation with tests, and migrated db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+  has_secure_password
+ 
+  validates :name, presence: true, length: { maximum: 255} 
+  validates :email, presence: true, length: { maximum: 255}, format:  { with: VALID_EMAIL_REGEX }, uniqueness:{ case_sensitive: false }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_085304) do
+ActiveRecord::Schema.define(version: 2019_04_23_092459) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.string "provider_id"
-    t.string "provider"
+    t.string "provider_id", default: "000001"
+    t.string "provider", default: "learnHELP"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
