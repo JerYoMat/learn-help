da6e53bc780818028bdc947d363f410ddce92e15
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 11:53:41 -0400

    login user on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to learnHELP!"
       redirect_to @user
     else
