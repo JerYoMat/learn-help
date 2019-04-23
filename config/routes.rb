f23baca009b3b074aab58c94456e455a29baafcf
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 11:03:08 -0400

    added routes for logging in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root 'static_pages#home'
   resources :units, only: [:index, :show]
   resources :users
@@ -12,4 +9,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
