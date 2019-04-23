bc51a124d6b7c30505739558e2be3d65829f296a
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 17:46:43 -0400

    added remember_digest column to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_092459) do
+ActiveRecord::Schema.define(version: 2019_04_23_202352) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_04_23_092459) do
     t.string "provider", default: "learnHELP"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
