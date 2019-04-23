0b2e56911317dd0a3e3e5b0b2a63f3dcc44eed36
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 17:49:35 -0400

    added methods to store and forget a new token -> @user.remember, @user.authenticated?, @user.forget, @user.new_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,39 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save { self.email = email.downcase }
   has_secure_password
- 
+
   validates :name, presence: true, length: { maximum: 255} 
   validates :email, presence: true, length: { maximum: 255}, format:  { with: VALID_EMAIL_REGEX }, uniqueness:{ case_sensitive: false }
   validates :password, presence: true, confirmation: true, length: { minimum: 6 }, allow_nil: true 
 
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? 
       BCrypt::Engine::MIN_COST :
       BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
   
+  def self.new_token
+    SecureRandom.urlsafe_base64  
+  end 
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+
+  def forget 
+    update_attribute(:remember_digest, nil)
+  end 
+
 end
