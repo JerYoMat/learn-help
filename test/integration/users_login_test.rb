2c143ec0594402ebb0c69e0e3e8815a1f8c178b7
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 11:51:22 -0400

    added checks for being logged in to signup and login actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,7 +16,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: @user.email, password:"password1" } }
+    assert is_logged_in?
     assert_redirected_to user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end 
 
 end
