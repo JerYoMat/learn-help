b8e83172e8d261746486f3e0f77435c2bedbe47c
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 02:05:24 -0400

    added routes for units, credits, code of conduct

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  resource :units, only: [:index, :show]
+  get '/credits', to: 'static_pages#credits'
+  get '/code_of_conduct', to: 'static_pages#code_of_conduct'
   get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#new'
 end
