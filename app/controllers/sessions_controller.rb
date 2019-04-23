a74de6afe822102205aeabb824388f9a76906cfc
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 19:56:40 -0400

    added oauth to local env, and set config vars on heroku attempting push

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class SessionsController < ApplicationController
   def new
   end
@@ -15,12 +16,51 @@ class SessionsController < ApplicationController
     end
   end
 
+
+  def facebook_create
+    @user = User.find_or_create_by(provider_id: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.provider_id = auth['uid']
+      pwd = SecureRandom.hex #To bypass validations for password set up in user model
+      u.password = pwd 
+      u.password_confirmation = pwd
+    end
+    
+    log_in(@user)
+    redirect_to @user
+ 
+  end 
+
+  def github_create 
+    #Github OmniAuth Implementation 
+    
+    @user = User.find_or_create_by(uid: auth['uid']) do |user|  
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["extra"]["raw_info"]["login"] 
+      user.email = Faker::Internet.email
+      pwd = SecureRandom.hex #To bypass validations for password set up in user model
+      user.password = pwd 
+      user.password_confirmation = pwd
+    end
+
+    log_in(@user)
+    redirect_to @user
+  end 
+
+
+  
   def destroy
     log_out if logged_in?
     redirect_to root_url
   end
 
-
+  private
+ 
+  def auth
+    request.env['omniauth.auth']
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get '/auth/facebook/callback' => 'sessions#facebook_create'
 end
