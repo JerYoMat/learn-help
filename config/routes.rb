84008f90629a3326dc0de1237171f797dd9b6eee
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 22:49:40 -0400

    added github callback route

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get '/auth/facebook/callback' => 'sessions#facebook_create'
+  get '/auth/github/callback' => 'sessions#github_create'
 end
