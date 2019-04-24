07a0c5ce3383698644a0848f4eb6309570d5b9dc
JerYOMat <jeremy.emata@gmail.com>
2019-04-24 13:54:35 -0400

    added tests for requiring login and correct user to edit or update user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,55 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  setup do 
+    @user = users(:test_user_one)
+    @other_user = users(:test_user_two)
+  end 
+
+  teardown do 
+    @user = nil
+  end 
+  
+  test 'should get new' do
     get signup_path
     assert_response :success
   end
+
+  test 'redirect away from edit when not logged in' do
+    get edit_user_path(@user)
+    assert flash[:danger],'Please log in.'
+    assert_redirected_to login_url
+  end 
+
+  test 'redirect away from update when not logged in' do
+    patch user_path(@user), params: {
+      user: { 
+        name: @user.name,
+        email: @user.email 
+      } 
+    }
+    assert flash[:danger],'Please log in.'
+    assert_redirected_to login_url
+  end 
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: {
+      user: { 
+        name: @user.name,
+        email: @user.email
+      }
+    }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+
 end
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:test_user_one)
+    log_in_as(@user)
+  end 
+  teardown do 
+    Rails.cache.clear
+  end 
+
+  test 'unsuccessful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: {
+      user: { 
+        name:  '',                         email: 'foo@invalid',              password:'foo',                    password_confirmation: 'bar' 
+      } 
+    }
+    assert_template 'users/edit'
+    assert_select 'div.alert', text: 'The form contains 5 errors.'
+  end
+
+  test 'successful edit' do 
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: {
+      user: { 
+        name:  'My new name',
+        email: 'foo@valid.com',
+        password:'foobars',                
+        password_confirmation: 'foobars' 
+      } 
+    }
+    assert_not flash.empty?
+    assert_redirected_to @user 
+    @user.reload 
+    assert_equal('My new name', @user.name)
+    assert_equal('foo@valid.com', @user.email)
+  end 
+end
