dbeab5fa4ed976ecfbdf4d92551e5a647ac3d1c7
JerYOMat <jeremy.emata@gmail.com>
2019-04-23 22:50:02 -0400

    added rails middlware and specified scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user,read:org"
+end
