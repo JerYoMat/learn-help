2af2422e8f5f0b85e9239dc060a204943de5f42c
JerYOMat <jeremy.emata@gmail.com>
2019-04-30 17:43:02 -0400

    added routes for topics, tips and tip_ratings,removed help and contact routes - not in MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  resources :units, only: [:index, :show]
+  resources :topics
+  resources :tips 
+  resources :tip_ratings 
   resources :users
-  get '/contact', to: 'static_pages#contact'
-  get '/credits', to: 'static_pages#credits'
+  get '/acknowledgements', to: 'static_pages#credits'
   get '/code_of_conduct', to: 'static_pages#code_of_conduct'
   get '/about', to: 'static_pages#about'
-  get '/help', to: 'static_pages#help'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get    '/login',   to: 'sessions#new'
