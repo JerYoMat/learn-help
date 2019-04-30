4b1eb060023faf96fbb1985daf46e5aa841d0a6d
JerYOMat <jeremy.emata@gmail.com>
2019-04-29 20:06:49 -0400

    added testing for friendly forwarding

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -8,13 +8,22 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   teardown do 
     Rails.cache.clear
   end 
+  
+  test 'user edit with friendly forwarding' do 
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+  end 
 
   test 'unsuccessful edit' do
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: {
       user: { 
-        name:  '',                         email: 'foo@invalid',              password:'foo',                    password_confirmation: 'bar' 
+        name:  '',
+        email: 'foo@invalid',
+        password:'foo',
+        password_confirmation: 'bar' 
       } 
     }
     assert_template 'users/edit'
