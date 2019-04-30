373ca92ae4f1521cb7861a1667e79456c4b222ed
JerYOMat <jeremy.emata@gmail.com>
2019-04-30 18:54:22 -0400

    added bootcamp info to user - will need default values to not break for non camp students in current implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save { self.email = email.downcase }
   has_secure_password
-
+  validates :bootcamp_student, presence: true 
+  validates :has_graduated, presence: true 
+  validates :camp_organization, presence: true 
   validates :name, presence: true, length: { maximum: 255} 
   validates :email, presence: true, length: { maximum: 255}, format:  { with: VALID_EMAIL_REGEX }, uniqueness:{ case_sensitive: false }
   validates :password, presence: true, confirmation: true, length: { minimum: 6 }, allow_nil: true 
