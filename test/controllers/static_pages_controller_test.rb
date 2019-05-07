6671dd9d1e0fb7927fea4d9a951295266bf851a8
JerYOMat <jeremy.emata@gmail.com>
2019-05-07 01:35:29 -0400

    removed tests for links that are not in mvp

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -17,21 +17,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
-  test "should get help" do
-    get help_path
-    assert_response :success
-    assert_select "title", "Help | #{@base_title}"
-  end
-
   test "should get code of conduct" do
     get code_of_conduct_path
     assert_response :success
     assert_select "title", "Code of Conduct | #{@base_title}"
-  end
-  test "should contact" do
-    get contact_path
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
   end
   
 
