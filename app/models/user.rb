e07d49e3e47a6fa776dc5e8d4dae48e23f9c8fe7
JerYOMat <jeremy.emata@gmail.com>
2019-05-07 03:31:29 -0400

    created helper methods and established associations -methods: follow, unfollow, following? -associations: expressed as active and passive relationships, the user that is following is the active party in the relationship -default name changes: followed changed to following

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,18 @@ class User < ApplicationRecord
   has_many :topics 
   has_many :tips 
   has_many :tip_ratings 
+  has_many :active_relationships, 
+    class_name:  "Relationship",
+    foreign_key: "follower_id",
+    dependent:   :destroy
+  has_many :passive_relationships, 
+    class_name:  "Relationship",
+    foreign_key: "followed_id",
+    dependent:   :destroy
   
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -43,5 +54,18 @@ class User < ApplicationRecord
   def feed 
     Tip.where('user_id = ?', id)
   end 
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 
 end
