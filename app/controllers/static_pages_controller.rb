29c9030693b8457652d8e653ec2a5b3ec556f39b
JerYOMat <jeremy.emata@gmail.com>
2019-05-07 01:10:14 -0400

    builds tip feed for logged in users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @tip = current_user.tips.build 
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end 
   end
 
   def about
